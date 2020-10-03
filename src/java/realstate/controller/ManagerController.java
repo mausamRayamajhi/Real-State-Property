b4e4061d39746294f7af152d3dae023c229ff120
Mausam rayamajhi <mausam495@gmail.com>
2020-10-03 12:05:48 +1000

    ManagerContoller added

--- src/java/realstate/controller/ManagerController.java
+++ src/java/realstate/controller/ManagerController.java
@@ -38,12 +38,6 @@ public class ManagerController {
     private ManagerEJB managerEJB;
 
 
-    
-//    @EJB
-//    private RentPropertyEJB rentPropertyEJB;
-//
-//    @EJB
-//    private SalePropertyEJB salePropertyEJB;
 
     
      @EJB
@@ -65,44 +59,7 @@ public class ManagerController {
 
         managerList = managerEJB.findManagers();
 
-//        Manager m1 = new Manager("Mausam ", "Rayamajhi", 1234578, 15624987, "mausam45@gmail.com", 0l);
-//        Manager m2 = new Manager("Prativa ", "Shah", 757527, 1562456987, "Prativa@gmail.com", 0l);
-//        Manager m3 = new Manager("Bikash ", "Shah", 12345678, 1562456987, "Bikash@gmail.com", 0l);
-//        Manager m4 = new Manager("Nabin ", "khatri", 12345678, 1562456987, "Nabin@gmail.com", 0l);
-//        Manager m5 = new Manager("Lonen ", "sky", 12345678, 1562456987, "Lonen@gmail.com", 0l);
-//
-//        Address a1 = new Address(100, "Taylors road", "Melbourne", 3038, "Australia");
-//        Address a2 = new Address(200, "Elizabet st", "Sydney", 3058, "Australia");
-//        Address a3 = new Address(300, "Footscray road", "Perth", 3098, "Australia");
-//
-//        RentProperty r1 = new RentProperty(1l, 1300l, true, "This is long description", "Apartment", 12, 23, a1);
-//        RentProperty r2 = new RentProperty(2l, 1250l, false, "This is long description", "Apartment", 1212, 21233, a2);
-//        RentProperty r3 = new RentProperty(3l, 1290l, true, "This is long description", "House", 1002, 2323, a3);
-//
-//        SaleProperty s1 = new SaleProperty(123l, 1l, "This is desc", "House", 12, 23, a3);
-//        SaleProperty s2 = new SaleProperty(123234l, 1l, "This is desc", "Apartment", 142, 223, a1);
-//        SaleProperty s3 = new SaleProperty(12367l, 1l, "This is desc", "House", 2, 23, a2);
-//
-//        Allocation al1 = new Allocation(1l, m1, r1, s1, LocalDateTime.now());
-//        Allocation al2 = new Allocation(2l, m2, r2, s2, LocalDateTime.now());
-//        Allocation al3 = new Allocation(3l, m3, r3, s3, LocalDateTime.now());
-//
-//        managerEJB.createManager(m1);
-//        managerEJB.createManager(m2);
-//        managerEJB.createManager(m3);
-//        managerEJB.createManager(m4);
-//        managerEJB.createManager(m5);
-//        rentPropertyEJB.createRentProperty(r1);
-//        rentPropertyEJB.createRentProperty(r2);
-//        rentPropertyEJB.createRentProperty(r3);
-//
-//        salePropertyEJB.createSaleProperty(s1);
-//        salePropertyEJB.createSaleProperty(s2);
-//        salePropertyEJB.createSaleProperty(s3);
-//
-//        allocationEJB.createAllocation(al1);
-//        allocationEJB.createAllocation(al2);
-//        allocationEJB.createAllocation(al3);
+
         return "listManager.xhtml";
     }
 
